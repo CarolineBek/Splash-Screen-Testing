5dea169b0a9c4d5fdbf78a81a0d8f47cd4a24150
CarolineBek <caroline.bekeng@pbe.oslo.kommune.no>
2026-09-21 15:27:15 +0200

    Add font configuration and embed script functions

---
+++ Font/font-config.js
@@ -0,0 +1,38 @@
+/*
+  DISCLAIMER: This example code is provided for educational and demonstration purposes.
+  It may not represent best practices for security and/or performance and is not intended for production use.
+*/
+function generateScriptConfig() {
+  window.storyMapsEmbedConfig = {
+    storyId: "749af21064e34f029bdd53946d9d941a",
+    rootNode: ".storymaps-root",
+      font: {
+        title: {
+          fontFamily: "Permanent Marker",
+          weight: {
+            normal: 400,
+            bold: 400
+          }
+        },
+        body: {
+          fontFamily: "Kalam",
+          weight: {
+            normal: 400,
+            bold: 700
+          }
+        }
+      }
+    }
+  }
+
+
+function createScriptedEmbed() {
+    const script = document.createElement('script');
+    script.id = 'embed-script';
+    script.src =`https://storymaps.arcgis.com/embed/view`;
+    document.body.appendChild(script);
+  }
+
+
+generateScriptConfig()
+createScriptedEmbed()
